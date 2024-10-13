f9364b974a948043490fec86ca0260bc321e58d6
Dmitry Shvetsov <shvetsov@wsplanet.ru>
2024-10-13 19:06:23 +0300

    m

--- car_service_final_task/1_upload_fill_table.sql
+++ car_service_final_task/1_upload_fill_table.sql
@@ -1,7 +1,7 @@
---  PostgreSQL 15.5
+-- PostgreSQL 15.5
 -- Здесь загрузка БД и заполнение полей.
--- Запросы update по заполнению пустых значений однотипные, все можно не просматривать.
--- До нормалзации у одного pin бывает до 12 card.
+-- Запросы update по заполнению пустых значений однотипные. Привожу часть, при необходимости отправлю все.
+-- Примечание: у одного pin бывает до 12 card.
 
 -- Создаю базу данных локально. ОС: Убунту.
 psql
@@ -9,7 +9,7 @@ create database car_service owner=postgres;
 -- Устанавливаю пароль
 \password postgres;
 
--- Поключаюсь к БД в DBeaver (В DBeaver: новое соединение - проверить тип БД, имя пользователя, пароль и т.д.
+-- Подключаюсь к БД в DBeaver (в DBeaver: новое соединение - проверить тип БД, имя пользователя, пароль и т.д.
 -- - тест соединения.)
 
 -- Создаю таблицу в БД.
@@ -37,17 +37,19 @@ date timestamp
 
  -- Импорт файла в DBeaver: в DB правой мышью - импорт - мастер.
 
--- Проверяю, что связка email-имя уникальна. Т.е. у одного email только одно имя (либо пусто).
+ -- На всякий случай создал копию исходной таблицы.
+create table copy_all_data_origin as
+select * from all_data ad
+
+-- Заполнение пустых значений.
+-- Использовался поиск уникальности заполняемого столбца для набора ключей. Например, email уникальны в разрезе name.
+-- Связка email-name уникальна, т.е. у одного email только одно значение name (либо пусто):
 select email, count(distinct name) count_name
 from all_data ad
 where name != '' and name is not null
 group by 1 order by 2 desc
 
--- На всякий случай создал копию исходной таблицы.
-create table copy_all_data_origin as
-select * from all_data ad
-
--- Заполняю имена по email:
+-- Заполняю name по email:
 update all_data as ad
 set name = subquery.name
 from(
@@ -57,116 +59,29 @@ where name != '' and email != ''
 and name is not null and email is not null
 ) AS subquery
 where ad.email = subquery.email
-and (ad.name is null or ad.name = '')
+and (ad.name is null or ad.name = '') -- На всякий случай, чтобы не переписать уже заполненные.
 
--- Заполнил email по именам.
-update all_data as ad
-set email = subquery.email
-from (
-select distinct name, email
-from all_data
-where name != '' and email != ''
-and name is not null and email is not null
-) AS subquery
-where ad.name = subquery.name
-and (ad.email is null or ad.email = '')
 
--- Проверяю, что одной почте соответствует 1 телефон.
-select email, count(distinct phone) count_phone
-from all_data ad
-where phone != '' and phone is not null
-group by 1 order by 2 desc
-
--- Заполняю телефоны по почтам.
-update all_data as ad
-set phone = subquery.phone
-from (
-select distinct email, phone
-from all_data
-where phone != '' and email != ''
-and phone is not null and email is not null
-) AS subquery
-where ad.email = subquery.email
-and (ad.phone is null or ad.phone = '')
-
--- Заполняю пароли.
-update all_data as ad
-set password = subquery.password
-from (
-select distinct email, password
-from all_data
-where password != '' and email != ''
-and password is not null and email is not null
-) AS subquery
-where ad.email = subquery.email
-and (ad.password is null or ad.password = '')
-
--- Оказалось, что одной почте соответствует 1 машина, поэтому заполняю car по email:
-update all_data as ad
-set car = subquery.car
-from (
-select distinct email, car
-from all_data
-where car != '' and email != ''
-and car is not null and email is not null
-) AS subquery
-where ad.email = subquery.email
-and (ad.car is null or ad.car = '')
-
--- Заполняю vin:
-update all_data as ad
-set vin = subquery.vin
-from (
-select distinct email, vin
-from all_data
-where vin != '' and email != ''
-and vin is not null and email is not null
-) AS subquery
-where ad.email = subquery.email
-and (ad.vin is null or ad.vin = '')
-
--- Аналогично заполняю car_number.
--- Заполняю color по vin.
-
--- Видно, что pin уникален для date, email, payment:
-select date, email, payment, count(distinct pin) cnt
-from all_data ad
-where date is not null
-and email != '' and email is not null
-and payment is not null
-and pin != '' and pin is not null
-group by 1,2,3 order by 4 desc
-
--- Заполняю pin:
-update all_data as ad
-set pin = subquery.pin
-from (
-select distinct date, email, payment, pin
-from all_data
-where date is not null
-and email != '' and email is not null
-and payment is not null
-and pin != '' and pin is not null
-) AS subquery
-where (ad.date = subquery.date and ad.email = subquery.email and ad.payment = subquery.payment)
-and (ad.pin is null or ad.pin = '')
-
+-- Ниже указываю только используемые связки ключей. Скрипты однотипные, все их не привожу.
+-- Заполняю телефоны по почтам. Пароли по email.
+-- Оказалось, что одной почте соответствует 1 машина, поэтому заполняю car по email.
+-- vin по email. Аналогично заполняю car_number.
+-- color по vin.
+-- pin уникален для набора из 3 ключей date, email, payment.
 -- Заполняю service_addr по w_name. Также дополняю по w_phone.
 -- Заполняю service по w_phone, потом по w_name.
 -- Заполняю w_name по w_phone.
 -- Заполняю w_exp по w_name. w_phone по w_name. wages по w_name.
 -- Заполняю card по date, email, payment. По date, payment.
 
--- Как полностью заполнить card я не нашел, т.к. даже по одной покупке (pin) бывает до 12 карт:
+-- Как полностью заполнить card я не нашел. По одной покупке (pin) бывает до 12 карт:
 select pin, count(distinct card) cnt
 from all_data ad
 where pin != '' and pin is not null
 and card != '' and card is not null
 group by 1 order by 2 desc
 
--- Заполняю payment по date, vin, pin. Но как было 31005 пустых, так и осталось.
-
--- Все pin восстановить не удалось, т.к. они не уникальны в разрезе date, name, vin, w_name
+-- Все pin восстановить не удалось, т.к. они не уникальны даже в разрезе date, name, vin, w_name
 select date, name, vin, w_name, count(distinct pin) cnt
 from all_data ad
 where date is not null
@@ -176,9 +91,58 @@ and vin is not null and vin != ''
 and w_name is not null and w_name != ''
 group by 1,2,3,4 order by 5 desc
 
+-- pin уникальны в разрезе mileage, vin, но запрос обновил 0 строк:
+update all_data as ad
+set pin = subquery.pin
+from (
+select distinct mileage, vin, pin
+from all_data
+where
+mileage is not null
+and vin != '' and vin is not null
+and pin != '' and pin is not null
+) AS subquery
+where (ad.mileage = subquery.mileage and ad.vin = subquery.vin)
+and (ad.pin is null or ad.pin = '')
+-- Набор date, vin также не уникален для pin. Видимо, это несколько ремонтов одной машины за 1 день.
+
 -- Заполнил mileage по date, pin, vin. Правда с null как было 10425, так и осталось.
 -- Заполнение card по date, name, pin, card не снизило число null.
 
 -- Итого полностью не заполнились: card, payment (считаем за неудачные оплаты), pin, mileage.
 
 -- Схема БД: https://drawdb.vercel.app/editor?shareId=74a1aef3778dd69752bc7b943428fc78
+
+
+-- Примечание. Гипотетически можно использовать следующий метод, но он чреват ошибками и дал бы небольшое заполнение.
+-- У некоторых наборов ключей заполняемые значения неуникальны. Тогда мы отсекаем набор ключей, где
+-- заполняемые значения неуникальны.
+-- Например, по связке vin-pin payment был неуникален в 78 случаях. Мы их отсекаем:
+with vin_pin as (
+select
+vin
+,pin
+,count(distinct payment) cnt
+from all_data
+where
+vin != '' and vin is not null
+and pin != '' and pin is not null
+and payment is not null
+group by 1,2
+having count(distinct payment) = 1 -- Оставляем уникальные связки.
+order by 3 desc -- Сортировка используется при первоначальной проверке уникальности связки.
+)
+update all_data as ad
+set payment = subquery.payment
+from (
+select
+distinct
+ad1.vin
+,ad1.pin
+,ad1.payment
+from all_data ad1
+join vin_pin using(vin, pin)
+) AS subquery
+where ad.vin = subquery.vin
+and ad.pin = subquery.pin
+and ad.payment is null
--- car_service_final_task/2_db_schema.sql
+++ car_service_final_task/2_db_schema.sql
@@ -1,2 +1,78 @@
 -- Схема БД: https://drawdb.vercel.app/editor?shareId=74a1aef3778dd69752bc7b943428fc78
 
+-- Создаю таблицы согласно схеме:
+create table workers as
+(
+select
+row_number() OVER (ORDER by w_phone) id
+,unique_w.w_name
+,unique_w.w_exp
+,unique_w.w_phone
+,unique_w.wages
+from (
+select distinct
+w_name
+,w_exp
+,w_phone
+,wages
+from all_data ad
+) unique_w
+)
+
+create table cars as
+(
+select
+row_number() OVER (ORDER by vin) id
+,subquery.*
+from (
+select distinct
+car
+,vin
+,car_number
+,color
+from all_data ad
+) subquery
+)
+
+create table services as
+(
+select
+row_number() OVER (ORDER by service_addr) id
+,subquery.*
+from (
+select distinct
+service
+,service_addr
+from all_data ad
+) subquery
+)
+
+create table orders as
+(
+select
+row_number() OVER (ORDER by pin) id
+,subquery.*
+from (
+select distinct
+card
+,payment
+,pin
+,mileage
+from all_data ad
+) subquery
+)
+
+create table clients as
+(
+select
+row_number() OVER (ORDER by email) id
+,subquery.*
+from (
+select distinct
+name
+,phone
+,email
+,password
+from all_data ad
+) subquery
+)
